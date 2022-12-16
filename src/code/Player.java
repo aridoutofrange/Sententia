7378acda0773b9b64f60bea50403a42ef009b445
nurhikam <nurhikam65@gmail.com>
2022-12-16 14:49:35 +0700

    menambahkan file Player.java

---
+++ src/code/Player.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code;
+
+/**
+ *
+ * @author Nurhikam
+ */
+public class Player {
+	
+	int position;
+	
+	public Player( int position )
+	{
+		this.position = position;
+	}
+	
+}
