810b7c714626769c9c8c124bb2948527e74238ab
aridoutofrange <ramadhirraazzahraputri@upi.edu>
2023-01-02 11:37:42 +0700

    win condition

--- src/main/java/com/ramadhira/Sententia/GameBoard.java
+++ src/main/java/com/ramadhira/Sententia/GameBoard.java
@@ -2,6 +2,9 @@ package com.ramadhira.Sententia;
 
 import java.util.Random;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class GameBoard {
 	public static final int ROWS = 10;
 	public static final int COLS = 10;
@@ -51,21 +54,36 @@ public class GameBoard {
         Character ple = App.players[player];
         int dice = rn.nextInt(6)+1;
         latestDice = dice;
-        App.players[player].move(dice);
 
-        // register each skill of characters
-        if(ple.streak.size() == 2 && ple.streak.get(ple.streak.size()-1) && ple.name.equals("Ralune") && ple.isCanUsePassiveCharacter){
-            App.players[player].position = ple.position + 10;
-            App.players[player].isCanUsePassiveCharacter = false;
-        }
+        boolean move = true;
+        while(App.players[player].position != 100 && move){
 
-        //tile rules
-        Tile tile=tiles[ple.position-1];
-        if(tile.snakeLadder!=null){
-            int[] sl = tile.snakeLadder;
-            if((sl[0]>sl[1]&&ple.isCanUseSnake) || (sl[0]<sl[1]&&ple.isCanUseLadder)){
-                App.players[player].position = sl[1];
+            App.players[player].move(dice);
+    
+            // register each skill of characters
+            if(ple.streak.size() == 2 && ple.streak.get(ple.streak.size()-1) && ple.name.equals("Ralune") && ple.isCanUsePassiveCharacter){
+                App.players[player].position = ple.position + 10;
+                App.players[player].isCanUsePassiveCharacter = false;
             }
+    
+            //tile rules
+            Tile tile=tiles[ple.position-1];
+            if(tile.snakeLadder!=null){
+                int[] sl = tile.snakeLadder;
+                if((sl[0]>sl[1]&&ple.isCanUseSnake) || (sl[0]<sl[1]&&ple.isCanUseLadder)){
+                    App.players[player].position = sl[1];
+                }
+            }
+            move = false;
+        }
+        if(App.players[player].position == 100 ){
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setContentText(App.players[player].name + " win the game");
+            alert.show();
+            try{
+
+                App.setRoot(new GUIMainMenu().getGUI());
+            }catch(Exception e){System.out.println(e);}
         }
     }
 
